00f8128c91fdfc7d806ac3161dcf11540c8d5635
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-02-18 15:32:06 +0300

    add model

--- src/by/matrosov/filemanager/FileManager.java
+++ src/by/matrosov/filemanager/FileManager.java
@@ -3,57 +3,89 @@ package by.matrosov.filemanager;
 import javax.swing.*;
 import javax.swing.event.TreeModelListener;
 import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
+import java.awt.*;
 import java.io.File;
+import java.util.Enumeration;
+import java.util.NoSuchElementException;
 
 public class FileManager extends JPanel{
 
     private JTree tree;
 
-    private static class FileTreeModel implements TreeModel{
+    private static class FileTreeModel implements TreeNode{
 
-        public FileTreeModel(File[] roots) {
+        private File file;
+        private File[] children;
+        private TreeNode parent;
+        private boolean isFileSystemRoot;
 
+        public FileTreeModel(File file, TreeNode parent, boolean isFileSystemRoot) {
+            this.file = file;
+            this.parent = parent;
+            this.isFileSystemRoot = isFileSystemRoot;
+            this.children = this.file.listFiles();
+            if (this.children == null)
+                this.children = new File[0];
+        }
+
+        FileTreeModel(File[] children) {
+            this.file = null;
+            this.parent = null;
+            this.children = children;
         }
 
         @Override
-        public Object getRoot() {
-            return null;
+        public TreeNode getChildAt(int childIndex) {
+            return new FileTreeModel(this.children[childIndex], this, this.parent == null);
         }
 
         @Override
-        public Object getChild(Object parent, int index) {
-            return null;
+        public int getChildCount() {
+            return this.children.length;
         }
 
         @Override
-        public int getChildCount(Object parent) {
-            return 0;
+        public TreeNode getParent() {
+            return this.parent;
         }
 
         @Override
-        public boolean isLeaf(Object node) {
-            return false;
+        public int getIndex(TreeNode node) {
+            FileTreeModel ftn = (FileTreeModel) node;
+            for (int i = 0; i < this.children.length; i++) {
+                if (ftn.file.equals(this.children[i]))
+                    return i;
+            }
+            return -1;
         }
 
         @Override
-        public void valueForPathChanged(TreePath path, Object newValue) {
-
+        public boolean getAllowsChildren() {
+            return true;
         }
 
         @Override
-        public int getIndexOfChild(Object parent, Object child) {
-            return 0;
+        public boolean isLeaf() {
+            return (this.getChildCount() == 0);
         }
 
         @Override
-        public void addTreeModelListener(TreeModelListener l) {
-
-        }
-
-        @Override
-        public void removeTreeModelListener(TreeModelListener l) {
-
+        public Enumeration children() {
+            final int elementCount = this.children.length;
+            return new Enumeration<File>() {
+                int count = 0;
+                public boolean hasMoreElements() {
+                    return this.count < elementCount;
+                }
+                public File nextElement() {
+                    if (this.count < elementCount) {
+                        return FileTreeModel.this.children[this.count++];
+                    }
+                    throw new NoSuchElementException("Vector Enumeration");
+                }
+            };
         }
     }
 
