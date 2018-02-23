0bc0f726a0942dd4c36bdd55f1cad1d539542472
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-02-23 16:43:29 +0300

    gui impl

--- src/by/matrosov/filemanager/FileManager.java
+++ src/by/matrosov/filemanager/FileManager.java
@@ -8,53 +8,18 @@ import java.io.File;
 
 public class FileManager extends JPanel{
 
-    private JTree tree;
-    private static FileSystemView fsv = FileSystemView.getFileSystemView();
-    private static boolean useSystemLookAndFeel = true;
-
     private FileManager() {
-        DefaultMutableTreeNode treeNode = new DefaultMutableTreeNode();
-        DefaultTreeModel model = new DefaultTreeModel(treeNode);
-
-        File[] roots = fsv.getRoots();
-        for (File f : roots) {
-            DefaultMutableTreeNode node = new DefaultMutableTreeNode(f);
-            treeNode.add(node);
-
-            File[] files = fsv.getFiles(f, true);
-            for (File file : files) {
-                if (file.isDirectory())
-                    node.add(new DefaultMutableTreeNode(file));
-            }
-        }
-
-        tree = new JTree();
-        tree.setModel(model);
-
-        //Create the scroll and add the tree to it.
-        JScrollPane scroll = new JScrollPane(tree);
-        add(scroll);
     }
 
     private static void createAndShowGUI(){
-        if (useSystemLookAndFeel) {
-            try {
-                UIManager.setLookAndFeel(
-                        UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e) {
-                System.err.println("Couldn't use system look and feel.");
-            }
-        }
-
-        //Create and set up the window.
         JFrame frame = new JFrame("FileManager");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        //Add content to the window.
-        frame.add(new FileManager());
+        frame.setContentPane(new FileManager());
 
-        //Display the window.
         frame.pack();
+        frame.setLocationByPlatform(true);
+        frame.setMinimumSize(frame.getSize());
         frame.setVisible(true);
     }
 
